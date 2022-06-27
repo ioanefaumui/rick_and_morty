873d3241e3e56cc5b4d41af590cc5a7b21ddc5bc
faumui <ioane.faumui@queroquitar.com.br>
2022-06-27 17:34:26 -0300

    feat: add context and graphql providers

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import Header from "../components/Header";
+import "../styles/globals.css";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { FavoriteProvider } from "../providers/Favorites";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <FavoriteProvider>
+        <ApolloProvider client={client}>
+          <div className="overflow-hidden relative h-auto pb-20">
+            <Header />
+            <Component {...pageProps} />
+          </div>
+        </ApolloProvider>
+      </FavoriteProvider>
+    </>
+  );
+};
+
+export default MyApp;
